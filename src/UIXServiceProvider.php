51528eb4b28746e8e7b9304714af948b296337bc
Raf <raftalks@gmail.com>
2016-04-21 15:22:46 +0500

    add initial files

---
+++ src/UIXServiceProvider.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: raf
+ * Date: 4/21/16
+ * Time: 3:12 PM
+ */
+
+namespace Raftalks\UIX;
+
+
+use Illuminate\Support\ServiceProvider;
+
+class UIXServiceProvider extends ServiceProvider
+{
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        // TODO: Implement register() method.
+    }
+}
